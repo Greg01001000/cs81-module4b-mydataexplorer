7878fe4c72ef4620b7ca0b2dd822ed680f88ba49
Greg Hagen <gr-a@outlook.com>
2025-07-09 00:27:11 -0700

    Added data & predictions

---
+++ myDataJournal.js
@@ -0,0 +1,26 @@
+
+const weekData = [
+  { day: "Monday", sleepHours: 8, screenTime: 10, mood: "fair", caffeineIntake: 0, focusLevel: 9 },
+  { day: "Tuesday", sleepHours: 7, screenTime: 8, mood: "poor", caffeineIntake: 1, focusLevel: 8 },
+  { day: "Wednesday", sleepHours: 6, screenTime: 12, mood: "fair", caffeineIntake: 1, focusLevel: 9 },
+  { day: "Thursday", sleepHours: 7, screenTime: 7, mood: "good", caffeineIntake: 0, focusLevel: 10 },
+  { day: "Friday", sleepHours: 6, screenTime: 12, mood: "excellent", caffeineIntake: 1, focusLevel: 9 },
+  { day: "Saturday", sleepHours: 5, screenTime: 6, mood: "good", caffeineIntake: 0, focusLevel: 10 },
+  { day: "Sunday", sleepHours: 7, screenTime: 11, mood: "excellent", caffeineIntake: 1, focusLevel: 9 }
+]
+
+// PREDICTIONS: I predict that the most screentime will be on either Wednesday 
+// or Friday because I usually have fewer issues requiring my attention late in
+// the week, aside from a weekly Thursday meeting, and I tend to be caught up
+// on sleep late in the week.
+// I predict that my best focus day is Sunday because I am motivated by 
+// classwork deadlines.
+// I don't ingest much caffeine, but sometimes in the afternoon I get sleepy
+// and my productivity drops. If I try to power through without caffeine, I 
+// usually fall asleep while sitting up. So usually as soon as I notice that
+// I'm getting sleepy, I get a cup of strong, black tea, which usually wakes me
+// up and restores my productivity. So I believe my limited intake helps, even
+// if it might not correlate with overall higher-focus or better-mood days in
+// my made-up data. But since you asked, I'll just say, sure, I think caffeine
+// is helping.
+
